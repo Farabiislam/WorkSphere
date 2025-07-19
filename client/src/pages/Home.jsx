b21ab942ab20e14ac10f34a72d538c214f91a485
suraiya-jahan-bhuiyan-sraboni <suraiya.jaha.bhuiyan.12@gmail.com>
2025-07-19 16:21:54 +0600

    client section done

--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -11,13 +11,15 @@ import Autoplay from "embla-carousel-autoplay";
 import Header from "../components/Home/Header";
 import Solutions from "../components/Home/Solutions";
 import Selection from "../components/Home/Selection";
+import ClientSection from "../components/Home/ClientSection";
 
 const Home = () => {
   return (
     <div className="">
      <Header/>
       <Solutions />
-      <Selection/>
+      <Selection />
+      <ClientSection/>
     </div>
   );
 };
