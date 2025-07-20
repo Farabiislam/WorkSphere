a6b62edfa9c3423ef39054c6b59cf17538f11c4f
farabi islam <farabii099@gmail.com>
2025-07-20 15:37:34 +0600

    animation removed

--- client/src/components/Home/ClientSection.jsx
+++ client/src/components/Home/ClientSection.jsx
@@ -1,99 +1,94 @@
-import React, { useEffect } from "react";
+import React from 'react'
 import {
-  Carousel,
-  CarouselContent,
-  CarouselItem,
-  CarouselNext,
-  CarouselPrevious,
+    Carousel,
+    CarouselContent,
+    CarouselItem,
+    CarouselNext,
+    CarouselPrevious,
 } from "@/components/ui/carousel";
 import { Star } from "lucide-react";
 import Autoplay from "embla-carousel-autoplay";
-import AOS from "aos";
-import "aos/dist/aos.css";
 
 const testimonials = [
-  {
-    initials: "EC",
-    name: "Emily Chen",
-    role: "CEO",
-    company: "StartupHub",
-    color: "bg-yellow-400",
-  },
-  {
-    initials: "JS",
-    name: "John Smith",
-    role: "CTO",
-    company: "TechHive",
-    color: "bg-green-500",
-  },
-  {
-    initials: "AK",
-    name: "Ayesha Khan",
-    role: "HR Manager",
-    company: "PeopleFirst",
-    color: "bg-blue-500",
-  },
+    {
+        initials: "EC",
+        name: "Emily Chen",
+        role: "CEO",
+        company: "StartupHub",
+        color: "bg-yellow-400",
+    },
+    {
+        initials: "JS",
+        name: "John Smith",
+        role: "CTO",
+        company: "TechHive",
+        color: "bg-green-500",
+    },
+    {
+        initials: "AK",
+        name: "Ayesha Khan",
+        role: "HR Manager",
+        company: "PeopleFirst",
+        color: "bg-blue-500",
+    },
 ];
 const ClientSection = () => {
-  useEffect(() => {
-    AOS.init({ duration: "1000" });
-  });
   return (
-    <section className="py-16 px-4 md:px-10  text-center">
-      <div className="max-w-3xl mx-auto">
-        <div data-aos="zoom-in" data-aos-easing="ease-in-sine">
-          <h2 className="text-3xl font-bold  mb-2">What Our Clients Say</h2>
-          <p className="text-muted-foreground mb-10">
-            Trusted by thousands of companies worldwide to manage their most
-            valuable asset – their people.
-          </p>
-        </div>
-        <Carousel
-          data-aos="fade-up"
-          data-aos-easing="ease-in-sine"
-          className="w-full max-w-xl mx-auto"
-          plugins={[
-            Autoplay({
-              delay: 2000,
-            }),
-          ]}
-        >
-          <CarouselContent>
-            {testimonials.map((item, i) => (
-              <CarouselItem key={i}>
-                <div className="bg-secondary rounded-xl p-6 shadow-md">
-                  <div className="flex justify-center mb-4">
-                    {Array(5)
-                      .fill(0)
-                      .map((_, i) => (
-                        <Star
-                          key={i}
-                          className="w-5 h-5 text-primary fill-primary  mx-0.5"
-                        />
-                      ))}
-                  </div>
-                  <div className="flex flex-col items-center gap-2">
-                    <div
-                      className={`w-12 h-12 rounded-full  font-semibold flex items-center justify-center ${item.color}`}
-                    >
-                      {item.initials}
-                    </div>
-                    <div>
-                      <h4 className="font-semibold ">{item.name}</h4>
-                      <p className="text-sm ">{item.role}</p>
-                      <p className="text-sm text-indigo-600 font-medium">
-                        {item.company}
-                      </p>
-                    </div>
-                  </div>
-                </div>
-              </CarouselItem>
-            ))}
-          </CarouselContent>
-        </Carousel>
-      </div>
-    </section>
-  );
-};
+      <section className="py-16 px-4 md:px-10  text-center">
+          <div className="max-w-3xl mx-auto">
+              <h2 className="text-3xl font-bold  mb-2">
+                  What Our Clients Say
+              </h2>
+              <p className="text-muted-foreground mb-10">
+                  Trusted by thousands of companies worldwide to manage their most
+                  valuable asset – their people.
+              </p>
 
-export default ClientSection;
+              <Carousel className="w-full max-w-xl mx-auto"
+              plugins={[
+                          Autoplay({
+                            delay: 2000,
+                          }),
+                        ]}
+              >
+                  <CarouselContent>
+                      {testimonials.map((item, i) => (
+                          <CarouselItem key={i}>
+                              <div className="bg-secondary rounded-xl p-6 shadow-md">
+                                  <div className="flex justify-center mb-4">
+                                      {Array(5)
+                                          .fill(0)
+                                          .map((_, i) => (
+                                              <Star
+                                                  key={i}
+                                                  className="w-5 h-5 text-primary fill-primary  mx-0.5"
+                                              />
+                                          ))}
+                                  </div>
+                                  <div className="flex flex-col items-center gap-2">
+                                      <div
+                                          className={`w-12 h-12 rounded-full  font-semibold flex items-center justify-center ${item.color}`}
+                                      >
+                                          {item.initials}
+                                      </div>
+                                      <div>
+                                          <h4 className="font-semibold ">
+                                              {item.name}
+                                          </h4>
+                                          <p className="text-sm ">{item.role}</p>
+                                          <p className="text-sm text-indigo-600 font-medium">
+                                              {item.company}
+                                          </p>
+                                      </div>
+                                  </div>
+                              </div>
+                          </CarouselItem>
+                      ))}
+                  </CarouselContent>
+              </Carousel>
+          </div>
+      </section>
+  )
+}
+
+export default ClientSection
--- client/src/components/Home/Selection.jsx
+++ client/src/components/Home/Selection.jsx
@@ -1,8 +1,7 @@
-import React, { useEffect } from 'react'
+import React from 'react'
 import { Button } from "@/components/ui/button";
 import { CheckIcon } from "lucide-react";
-import AOS from "aos";
-import "aos/dist/aos.css";
+
 const features = [
     {
         title: "Intuitive Interface",
@@ -29,66 +28,52 @@ const features = [
 
 
 const Selection = () => {
-          useEffect(()=>{
-              AOS.init({duration:"1000"});
-          })
   return (
-    <section data-aos="zoom-in" className="bg-secondary py-16 px-4 ">
-      <div className="grid grid-cols-1 md:grid-cols-2 gap-10 items-center">
-        {/* Left Side */}
-        <div data-aos="fade-up">
-          <h2 className="text-3xl font-bold mb-6">Why Choose WorkSphere?</h2>
+      <section className="bg-secondary py-16 px-4 ">
+          <div className="grid grid-cols-1 md:grid-cols-2 gap-10 items-center">
+              {/* Left Side */}
+              <div>
+                  <h2 className="text-3xl font-bold mb-6">
+                      Why Choose WorkSphere?
+                  </h2>
 
-          <ul data-aos="fade-up" className="space-y-6">
-            {features.map((feature, index) => (
-              <li key={index} className="flex items-start gap-4">
-                <div className="mt-1">
-                  <CheckIcon className="bg-blue-600 text-white rounded p-1 w-6 h-6" />
-                </div>
-                <div>
-                  <h3 className="font-semibold text-lg ">{feature.title}</h3>
-                  <p className=" text-sm text-muted-foreground">
-                    {feature.description}
-                  </p>
-                </div>
-              </li>
-            ))}
-          </ul>
+                  <ul className="space-y-6">
+                      {features.map((feature, index) => (
+                          <li key={index} className="flex items-start gap-4">
+                              <div className="mt-1">
+                                  <CheckIcon className="bg-blue-600 text-white rounded p-1 w-6 h-6" />
+                              </div>
+                              <div>
+                                  <h3 className="font-semibold text-lg ">
+                                      {feature.title}
+                                  </h3>
+                                  <p className=" text-sm text-muted-foreground">{feature.description}</p>
+                              </div>
+                          </li>
+                      ))}
+                  </ul>
 
-          <Button
-            data-aos="zoom-in"
-            className="mt-8 bg-blue-600 hover:bg-blue-700 text-white"
-          >
-            Learn More
-          </Button>
-        </div>
+                  <Button className="mt-8 bg-blue-600 hover:bg-blue-700 text-white">
+                      Learn More
+                  </Button>
+              </div>
 
-        {/* Right Side - Dashboard Preview */}
-        <div
-          data-aos="fade-right"
-          className="flex justify-center md:justify-end relative"
-        >
-          <div
-            data-aos="fade-down"
-            data-aos-delay="500"
-            data-aos-easing="ease-in-sine"
-            className="h-[300px] w-full border-30 border-white  bg-blue-600 rounded-xl shadow-lg flex items-center justify-center text-white text-xl font-semibold relative z-10"
-          >
-            Dashboard Preview
+              {/* Right Side - Dashboard Preview */}
+              <div className="flex justify-center md:justify-end relative">
+
+                  <div className="h-[300px] w-full border-30 border-white  bg-blue-600 rounded-xl shadow-lg flex items-center justify-center text-white text-xl font-semibold relative z-10">
+                      Dashboard Preview
+                  </div>
+
+                  <div style={{
+                      opacity: 0.4,
+                      background: "linear-gradient(90deg, #F16397 0%, #18D13A 100%)"
+                  }} className="absolute -top-10 -right-10 w-26 h-26 bg-purple-300 opacity-30 rounded-full z-10" />
+                  <div className="absolute -bottom-10 -left-10 w-20 h-20 opacity-35 bg-blue-300 rounded-full z-0" />
+              </div>
           </div>
-
-          <div
-            style={{
-              opacity: 0.4,
-              background: "linear-gradient(90deg, #F16397 0%, #18D13A 100%)",
-            }}
-            className="absolute -top-10 -right-10 w-26 h-26 bg-purple-300 opacity-30 rounded-full z-10"
-          />
-          <div className="absolute -bottom-10 -left-10 w-20 h-20 opacity-35 bg-blue-300 rounded-full z-0" />
-        </div>
-      </div>
-    </section>
-  );
+      </section>
+  )
 }
 
 export default Selection
