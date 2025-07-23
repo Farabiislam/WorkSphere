b8d0e80d2224577f2d6eac220239fa97abfdbe59
suraiya-jahan-bhuiyan-sraboni <suraiya.jaha.bhuiyan.12@gmail.com>
2025-07-24 05:46:54 +0600

    upddated

--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -1,13 +1,4 @@
 import React from "react";
-import { Button } from "@/components/ui/button";
-import {
-  Carousel,
-  CarouselContent,
-  CarouselItem,
-  CarouselNext,
-  CarouselPrevious,
-} from "@/components/ui/carousel";
-import Autoplay from "embla-carousel-autoplay";
 import Header from "../components/Home/Header";
 import Solutions from "../components/Home/Solutions";
 import Selection from "../components/Home/Selection";
